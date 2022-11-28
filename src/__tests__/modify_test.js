bb259d18e0dac186f45b824ae2438b3a7250851a
karthikv6 <44593407+karthikv6@users.noreply.github.com>
2022-11-27 20:21:40 -0800

    Update modify_test.js

--- src/__tests__/modify_test.js
+++ src/__tests__/modify_test.js
@@ -8,6 +8,9 @@ import { Profile, update_info_modal } from '../scripts/home';
 
 
 describe('Test modify profile', () => {
+    beforeAll(async () => {
+        await page.goto('http://127.0.0.1:5500/src/');
+    });
     let test_profile = new Profile(0, "title1", "tag1", "expDate1", "serialNum1", "note1");
     test('Before Modify', () => {
         expect(test_profile.title).toBe("title1");
@@ -17,9 +20,12 @@ describe('Test modify profile', () => {
         expect(test_profile.note).toBe("note1");
     });
     
-    const mod_profile = update_info_modal(test_profile);
+    //const mod_profile = update_info_modal(test_profile);
 
-    test('After Modify', () => {
+    test('After Modify', async () => {
 
+        const NEW_MODAL_BUTTON = await page.$('PROFILE_LIST')
+        console.log(NEW_MODAL_BUTTON)
+        
     });
 });
