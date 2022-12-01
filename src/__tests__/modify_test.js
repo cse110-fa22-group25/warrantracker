9ef97953508d7225d7550cfc90e5e47d5d320083
karthikv6 <44593407+karthikv6@users.noreply.github.com>
2022-11-30 17:17:47 -0800

    Completed Unit Test

--- src/__tests__/modify_test.js
+++ src/__tests__/modify_test.js
@@ -3,15 +3,14 @@
  */
 
 
-import { Profile, update_info_modal } from '../scripts/home';
-
-
+import {Profile, init, create_profile, get_profile_from_storage, update_info_modal, profile_list, delete_profile} from '../scripts/home';
+const puppeteer = require('puppeteer');
 
 describe('Test modify profile', () => {
-    beforeAll(async () => {
-        await page.goto('http://127.0.0.1:5500/src/');
-    });
+    localStorage.clear()
     let test_profile = new Profile(0, "title1", "tag1", "expDate1", "serialNum1", "note1");
+
+    //check values before modifying
     test('Before Modify', () => {
         expect(test_profile.title).toBe("title1");
         expect(test_profile.tag).toBe("tag1");
@@ -20,12 +19,22 @@ describe('Test modify profile', () => {
         expect(test_profile.note).toBe("note1");
     });
     
-    //const mod_profile = update_info_modal(test_profile);
+    
 
+    //tests after modifications
     test('After Modify', async () => {
+        //modify values
+        test_profile.title = "title2"
+        test_profile.tag = "tag2"
+        test_profile.exp_date = "expDate2"
+        test_profile.serial_num = "serialNum2"
+        test_profile.note = "note2"
 
-        const NEW_MODAL_BUTTON = await page.$('PROFILE_LIST')
-        console.log(NEW_MODAL_BUTTON)
+        expect(test_profile.title).toBe("title2");
+        expect(test_profile.tag).toBe("tag2");
+        expect(test_profile.exp_date).toBe("expDate2");
+        expect(test_profile.serial_num).toBe("serialNum2");
+        expect(test_profile.note).toBe("note2");
         
     });
 });
