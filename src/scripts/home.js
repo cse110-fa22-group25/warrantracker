399a746904a5f9edb8c567ad2c386449197f0622
Yong Liang <yoliang@ucsd.edu>
2022-12-01 21:19:52 -0800

    change plus icon

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -467,7 +467,9 @@ function display_selected_profile(profiles) {
         data-bs-target="#new-modal"
         class="card"
       >
-        <ion-icon class="position-absolute top-50 start-50 translate-middle" name="add-outline" id="plus-icon"></ion-icon>
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-plus-lg position-absolute top-50 start-50 translate-middle" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
+        </svg>
       </div>
     </div>
   `;
